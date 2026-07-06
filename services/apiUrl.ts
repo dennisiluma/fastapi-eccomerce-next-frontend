7f282fb3fc3b123d752c247fc26afca81ca47ea3
phegondev <phegondev@gmail.com>
2026-07-06 16:20:24 +0100

    added prod api url

--- services/apiUrl.ts
+++ services/apiUrl.ts
@@ -2,6 +2,13 @@
 
 
 export const API_CONFIG = {
+
+    // LOCAL
     BACKEND_URL: "http://localhost:8084",
     API_URL: "http://localhost:8084/api",
+
+
+    //PROD
+    // BACKEND_URL: "http://98.93.39.168:8084",
+    // API_URL: "http://98.93.39.168:8084/api",
 }
